07baafc781ce5c3de690d4c72916b71933b7c29d
liedsonnhapulo-maker <lnhapulo@byupathway.edu>
2026-07-21 00:22:15 +0200

    add hamburguer

--- chamber/scripts/main.mjs
+++ chamber/scripts/main.mjs
@@ -6,3 +6,46 @@ import { getSpotlights } from "./spotlight.mjs";
 getWeather();
 
 getSpotlights();
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+if (hamburger && navigation) {
+
+    hamburger.addEventListener("click", () => {
+
+        navigation.classList.toggle("open");
+
+        hamburger.classList.toggle("show");
+
+
+        const expanded = hamburger.classList.contains("show");
+
+        hamburger.setAttribute(
+            "aria-expanded",
+            expanded
+        );
+
+    });
+
+}
+
+
+
+const currentYear = document.querySelector("#currentyear");
+
+if (currentYear) {
+
+    currentYear.textContent =
+        new Date().getFullYear();
+
+}
+
+
+
+const lastModified = document.querySelector("#lastModified");
+
+if (lastModified) {
+
+    lastModified.textContent =
+        `Last Modification: ${document.lastModified}`;
+
+}
