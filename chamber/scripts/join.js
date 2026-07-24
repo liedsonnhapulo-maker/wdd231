b312e769855446d63e68dbcaede6e6839ea1b6aa
liedsonnhapulo-maker <lnhapulo@byupathway.edu>
2026-07-24 18:46:54 +0200

    add join page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -42,22 +42,6 @@ if (lastModified) {
 
 }
 
-const getString = window.location.search;
-console.log(getString)
-const myinfo = new URLSearchParams(window.location.search);
-const dateReceived = new Date().toLocaleString();
-
-document.querySelector('#message').innerHTML = `
-    <p>Thank you for your application, ${myinfo.get('fname')} ${myinfo.get('lname')}.</p>
-    <p>Your organizational title is ${myinfo.get('organization-title')}.</p>
-    <p>Organization Name: ${myinfo.get('organization')}</p>
-    <p>Membership Level: ${myinfo.get('membership')}</p>
-    <p>Business Description: ${myinfo.get('description')}</p>
-    <p>Your Phone: ${myinfo.get('phone')}</p>
-    <p>Your email is ${myinfo.get('email')}</p>
-    <p>Application received on: ${dateReceived}</p>
-`;
-
 const openButton1 = document.querySelector("#openButton1");
 const openButton2 = document.querySelector("#openButton2");
 const openButton3 = document.querySelector("#openButton3");
@@ -98,3 +82,20 @@ openButton4.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     dialogBox.close();
 });
+
+const getString = window.location.search;
+console.log(getString)
+const myinfo = new URLSearchParams(window.location.search);
+const dateReceived = new Date().toLocaleString();
+
+document.querySelector('#msg').innerHTML = `
+    <p>Thank you for your application, ${myinfo.get('fname')} ${myinfo.get('lname')}.</p>
+    <p>Your organizational title is ${myinfo.get('organization-title')}.</p>
+    <p>Organization Name: ${myinfo.get('organization')}</p>
+    <p>Membership Level: ${myinfo.get('membership')}</p>
+    <p>Business Description: ${myinfo.get('description')}</p>
+    <p>Your Phone: ${myinfo.get('phone')}</p>
+    <p>Your email is ${myinfo.get('email')}</p>
+    <p>Application received on: ${dateReceived}</p>
+`;
+
